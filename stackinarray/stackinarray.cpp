f4394ae36661274a7e5ad538a22461d5f42d6505
hi-lufz1 <latifufauzi@gmail.com>
2023-06-16 10:43:08 +0700

    Add project files.

---
+++ stackinarray/stackinarray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class StackArray {
+private:
+	int stack_array[5];
+	int top;
+
+public:
+	//construktor
+	stack_array() {
+		top = -1;
+
+	}
+
+	int push element()
+
+};
+
