ac50afe7bef0f0d3292b64e36e80f85ee076d971
hi-lufz1 <latifufauzi@gmail.com>
2023-06-16 10:59:24 +0700

    6

--- stackinarray/stackinarray.cpp
+++ stackinarray/stackinarray.cpp
@@ -36,10 +36,16 @@ public:
 		cout << "\nThe popped element is:" << stack_array[top] << endl;//step 2
 		top--;//step 3 decrement
 	}
-	//method
+	//method 
 	bool empty() {
 		return(top = -1);
+
 	}
 
+
 };
+
+int main() {
+
+}
 
